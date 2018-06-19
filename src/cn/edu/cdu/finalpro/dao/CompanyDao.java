2dc4b92b500b0063ec77319e8cf59d9acb0f39e9
peiyifan <609736241@qq.com>
2018-06-19 21:54:21 +0800

    o

--- src/cn/edu/cdu/finalpro/dao/CompanyDao.java
+++ src/cn/edu/cdu/finalpro/dao/CompanyDao.java
@@ -1,14 +1,10 @@
 package cn.edu.cdu.finalpro.dao;
 
 
+
 public class CompanyDao {
-	/**
-	 * @Copyright (C),2017,信息科学与工程学院软件工程系一班，
-	 * @FileName CompanyDao.java
-	 * @version 1.0
-	 * @Description:Dao层企业管理操作接口
-	 * @Author 编写人员填写
-	 * @Date 编写人员填写
-	 * 
-	 * */
+	
 }
+
+
+
